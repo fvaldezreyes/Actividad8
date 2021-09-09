5ad16911db055a7877b8b7018a24a8342dd09825
ferna <ferna@LAPTOP-35H7C6JK>
2021-09-08 21:20:02 -0500

    Agregar archivos de proyecto.

---
+++ Actividad8/Actividad8/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Actividad8
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnMsg1_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Mensaje","Bienvenido al mundo Xamarin.Forms " + txtNombre.Text,"Cancelar");
+        }
+    }
+    }
+
